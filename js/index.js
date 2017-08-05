8d94209a63fddc944d575672d5aa1a9ad0427dd6
cwong2015 <sirhc8002@gmail.com>
2017-08-05 10:24:21 -0400

    generated tictactoe table through javascript, removed HTML code

--- js/index.js
+++ js/index.js
@@ -1,5 +1,13 @@
+var buttonIDs = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+var trIDs = ["row_one", "row_two", "row_three"];
+
 $(document).ready(function(){
-  $("#test").click(function(){
-      alert("hello");
-  });
+    var currTrID;
+    for(var i = 0; i < buttonIDs.length; i++){
+        if(i%3 === 0){
+            $("#layout").append("<tr id = '" + trIDs[i/3] + "'></tr>");
+            currTrID = trIDs[i/3];
+        }
+        $("#" + currTrID).append("<td><button id ='" + buttonIDs[i] + "' class = 'myinvisible'></button></td>");
+    }
 });
