9d7aac509d5aa1669cb1c432e027907f22d7c863
cwong2015 <sirhc8002@gmail.com>
2017-08-06 00:41:57 -0400

    a lot of hardcoding going on for now but it will do

--- js/index.js
+++ js/index.js
@@ -2,6 +2,10 @@ var buttonIDs = ["one", "two", "three", "four", "five", "six", "seven", "eight",
 var trIDs = ["row_one", "row_two", "row_three"];
 var flags = [];
 var playerMark = "x";
+var opponentMark = "o";
+var playersTurn = false;
+var playerTrack = [0, 0, 0, 0, 0, 0, 0, 0]; // row1, row2, row3, col1, col2, col3, diag1, diag2
+var opponentTrack = [0, 0, 0, 0, 0, 0, 0, 0];
 
 for(var i = 0; i < 9; i++){
     flags[i] = "*";
@@ -23,12 +27,87 @@ $(document).ready(function(){
     $("#myModal").modal('show');
     $("#x").click(function(){
         playerMark = "x";
+        opponentMark = "o";
        $("#x").css({"background-color": "yellow"});
        $("#o").css({"background-color": "#32CD32"});
     });
     $("#o").click(function(){
         playerMark = "o";
+        opponentMark = "x";
        $("#x").css({'background-color': '#32CD32'});
        $("#o").css({'background-color': 'yellow'});
     });
+    if(!playersTurn){
+        placeMarker();
+        checkWin();
+    }
+    else{
+        aiMove();
+        checkWin();
+    }
 });
+
+function placeMarker(){
+    $("button").click(function(){
+       if(buttonIDs.includes(this.id)){
+           var index = buttonIDs.indexOf(this.id);
+           if(flags[index] === "*"){
+               flags[index] = playerMark;
+               $("#" + this.id).text("" + playerMark);
+               if(index === 0){
+                   playerTrack[0]++;
+                   playerTrack[3]++;
+                   playerTrack[6]++;
+               }
+               if(index === 1){
+                   playerTrack[0]++;
+                   playerTrack[4]++;
+               }
+               if(index === 2){
+                   playerTrack[0]++;
+                   playerTrack[5]++;
+                   playerTrack[7]++;
+               }
+               if(index === 3){
+                   playerTrack[1]++;
+                   playerTrack[3]++;
+               }
+               if(index === 4){
+                   playerTrack[1]++;
+                   playerTrack[4]++;
+                   playerTrack[6]++;
+                   playerTrack[7]++;
+               }
+               if(index === 5){
+                   playerTrack[1]++;
+                   playerTrack[5]++;
+               }
+               if(index === 6){
+                   playerTrack[2]++;
+                   playerTrack[3]++;
+                   playerTrack[7]++;
+               }
+               if(index === 7){
+                   playerTrack[2]++;
+                   playerTrack[4]++;
+               }
+               if(index === 8){
+                   playerTrack[2]++;
+                   playerTrack[6]++;
+               }
+           }
+       } 
+    });
+}
+
+function aiMove(){
+    // implement later
+}
+
+function checkWin(){
+    for(var i = 0; i < playerTrack.length; i++){
+        if(playerTrack[i] === 3){
+            alert("You win!");
+        }
+    }
+}
