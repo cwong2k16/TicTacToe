748ee755d3f0c6d643b35e5198d3a07068f44656
cwong2015 <sirhc8002@gmail.com>
2017-08-05 23:05:32 -0400

    pretty modal pops up asking user to choose marker

--- js/index.js
+++ js/index.js
@@ -1,7 +1,17 @@
 var buttonIDs = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
 var trIDs = ["row_one", "row_two", "row_three"];
+var flags = [];
+var playerMark = "x";
 
-$(document).ready(function(){
+for(var i = 0; i < 9; i++){
+    flags[i] = "*";
+}
+
+$(document).ready(function(){    
+    $("#myModal").modal({
+        backdrop: 'static',
+        keyboard: false
+    });
     var currTrID;
     for(var i = 0; i < buttonIDs.length; i++){
         if(i%3 === 0){
@@ -10,4 +20,15 @@ $(document).ready(function(){
         }
         $("#" + currTrID).append("<td><button id ='" + buttonIDs[i] + "' class = 'myinvisible'></button></td>");
     }
+    $("#myModal").modal('show');
+    $("#x").click(function(){
+        playerMark = "x";
+       $("#x").css({"background-color": "yellow"});
+       $("#o").css({"background-color": "#32CD32"});
+    });
+    $("#o").click(function(){
+        playerMark = "o";
+       $("#x").css({'background-color': '#32CD32'});
+       $("#o").css({'background-color': 'yellow'});
+    });
 });
